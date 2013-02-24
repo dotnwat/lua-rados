5b33f510403fab7ce510492ed5d2d4ed9548d5f7
Noah Watkins <noahwatkins@gmail.com>
2013-02-23 17:59:40 -0800

    inital commit: starting lua-rados project
    Basic build setup and wrapper around rados_version().
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

---
+++ lua_rados.c
@@ -0,0 +1,29 @@
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
+#include <rados/librados.h>
+
+static int lrad_version(lua_State *L)
+{
+	int major, minor, extra;
+
+	rados_version(&major, &minor, &extra);
+
+	lua_pushinteger(L, major);
+	lua_pushinteger(L, minor);
+	lua_pushinteger(L, extra);
+
+	return 3;
+}
+
+static const luaL_Reg radoslib_f[] = {
+	{"version", lrad_version},
+	{NULL, NULL}
+};
+
+LUALIB_API int luaopen_rados(lua_State *L)
+{
+	luaL_register(L, "rados", radoslib_f);
+	return 0;
+}
