d43546f193701eed55f3512f3f0e3e75b91ec6ff
Noah Watkins <noahwatkins@gmail.com>
2013-02-23 22:50:16 -0800

    core: add rados_create,conf_read_file,connect
    Along with basic tests and documentation.
    
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- lua_rados.c
+++ lua_rados.c
@@ -7,12 +7,20 @@
 
 #include <rados/librados.h>
 
+#define LRAD_TRADOS_T "Rados.RadosT"
+
+static inline rados_t *lrad_checkcluster(lua_State *L, int pos)
+{
+	return luaL_checkudata(L, pos, LRAD_TRADOS_T);
+}
+
 /**
 Get the version of librados.
 @function version
-@return major
-@return minor
-@return extra
+@return major version
+@return minor version
+@return extra version
+@usage major, minor, extra = rados.version()
  */
 static int lrad_version(lua_State *L)
 {
@@ -27,13 +35,124 @@ static int lrad_version(lua_State *L)
 	return 3;
 }
 
+/**
+Create handle for communicating with RADOS cluster.
+@function create
+@string id the user to connect as (optional, or nil)
+@usage cluster = rados.create()
+@usage cluster = rados.create(nil)
+@usage cluster = rados.create('admin')
+*/
+static int lrad_create(lua_State *L)
+{
+	const char *id = NULL;
+	rados_t *cluster;
+	int ret;
+
+	if (lua_gettop(L) > 0 && lua_type(L, 1) != LUA_TNIL) {
+		luaL_checktype(L, 1, LUA_TSTRING);
+		id = lua_tostring(L, 1);
+	}
+
+	cluster = lua_newuserdata(L, sizeof(*cluster));
+	luaL_getmetatable(L, LRAD_TRADOS_T);
+	lua_setmetatable(L, -2);
+
+	ret = rados_create(cluster, id);
+	if (ret) {
+		lua_pushnil(L);
+		lua_pushfstring(L, "%s", strerror(ret));
+		lua_pushinteger(L, ret);
+		return 3;
+	}
+
+	return 1;
+}
+
+/**
+@type Cluster
+ */
+
+/**
+Configure the cluster handle using a Ceph config file.
+@function conf_read_file
+@string file path to configuration file (optional, or nil)
+@return 0 on success, nil otherwise
+@return error message and retval if failed
+@usage cluster:conf_read_file()
+@usage cluster:conf_read_file(nil)
+@usage cluster:conf_read_file('/path/to/ceph.conf')
+ */
+static int lrad_conf_read_file(lua_State *L)
+{
+	rados_t *cluster = lrad_checkcluster(L, 1);
+	const char *conf_file = NULL;
+	int ret;
+
+	if (lua_gettop(L) > 1 && lua_type(L, 2) != LUA_TNIL) {
+		luaL_checktype(L, 2, LUA_TSTRING);
+		conf_file = lua_tostring(L, 2);
+	}
+
+	ret = rados_conf_read_file(*cluster, conf_file);
+	if (ret) {
+		lua_pushnil(L);
+		lua_pushfstring(L, "%s", strerror(ret));
+		lua_pushinteger(L, ret);
+		return 3;
+	}
+
+	lua_pushinteger(L, ret);
+	return 1;
+}
+
+/**
+Connect to the cluster.
+@function connect
+@return 0 on success, nil otherwise
+@return error message and retval if failed
+@usage cluster:connect()
+@usage status, errstr, ret = cluster:connect()
+ */
+static int lrad_connect(lua_State *L)
+{
+	rados_t *cluster = lrad_checkcluster(L, 1);
+	int ret;
+
+	ret = rados_connect(*cluster);
+	if (ret) {
+		lua_pushnil(L);
+		lua_pushfstring(L, "%s", strerror(-ret));
+		lua_pushinteger(L, ret);
+		return 3;
+	}
+
+	lua_pushinteger(L, ret);
+	return 1;
+}
+
+static const luaL_Reg clusterlib_m[] = {
+	{"conf_read_file", lrad_conf_read_file},
+	{"connect", lrad_connect},
+	{NULL, NULL}
+};
+
 static const luaL_Reg radoslib_f[] = {
 	{"version", lrad_version},
+	{"create", lrad_create},
 	{NULL, NULL}
 };
 
 LUALIB_API int luaopen_rados(lua_State *L)
 {
+	/* setup rados_t userdata type */
+	luaL_newmetatable(L, LRAD_TRADOS_T);
+	lua_pushvalue(L, -1);
+	lua_setfield(L, -2, "__index");
+	luaL_register(L, NULL, clusterlib_m);
+	lua_pop(L, 1);
+
 	luaL_register(L, "rados", radoslib_f);
+
 	return 0;
 }
